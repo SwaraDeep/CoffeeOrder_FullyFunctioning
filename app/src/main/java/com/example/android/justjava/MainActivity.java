aa556f2626e572e0fad6f5b70e00d5fea448a1cf
Swara Deep <swaradeepneeli@gmail.com>
2018-07-09 21:22:11 +0530

    coffee ordering app fully functioning final version

---
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -0,0 +1,87 @@
+package com.example.android.justjava;
+
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.TextView;
+
+/**
+ * This app displays an order form to order coffee.
+ */
+public class MainActivity extends AppCompatActivity {
+    int coffees = 0;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * This method is called when the order button is clicked.
+     */
+    public void submitOrder(View view) {
+        EditText name = (EditText) findViewById(R.id.name);
+        String names = name.getText().toString();
+        CheckBox cream = findViewById(R.id.cream);
+        boolean check = cream.isChecked();
+        CheckBox choco = findViewById(R.id.choco);
+        boolean chocolate = choco.isChecked();
+        String hasCream, hasChoco;
+        int coffeePrice = 0, coffeePrices = 0;
+        if(chocolate == true) {
+            hasChoco = "Yes";
+            coffeePrice = coffees * 1;
+        }
+        else {
+            hasChoco = "No";
+        }
+        if(check == true) {
+            hasCream = "Yes";
+            coffeePrices = (coffees * 2);
+        }
+        else {
+            hasCream = "No";
+        }
+        displayMessage(createOrderSummary(hasCream, hasChoco, names, coffeePrice, coffeePrices));
+    }
+
+    private String createOrderSummary(String hasCream, String hasChoco, String name, int coffeesPrice, int coffeesPrices) {
+        int coffeesTotalPrice = (coffees * 5) + coffeesPrice + coffeesPrices;
+        String summary = "Name: "+ name + "\nQuantity: " + coffees + "\nCream: " + hasCream + "\nChocolate: " + hasChoco + "\nPrice: $" + coffeesTotalPrice + "\nThankyou!";
+        return summary;
+    }
+
+    /**
+     * This method displays the given quantity value on the screen.
+     */
+    private void display(int number) {
+        TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
+        quantityTextView.setText("" + number);
+    }
+
+
+    public void incr (View view) {
+        display(coffees += 1);
+    }
+    public void decr(View view) {
+        if(coffees == 0) {
+            coffees = 0;
+        }
+        else{
+            coffees -= 1;
+        }
+        display(coffees);
+    }
+    /**
+     * This method displays the given text on the screen.
+     */
+    private void displayMessage(String message) {
+        TextView priceTextView = (TextView) findViewById(R.id.order_summary_text_view);
+        priceTextView.setText(message);
+    }
+
+
+}
